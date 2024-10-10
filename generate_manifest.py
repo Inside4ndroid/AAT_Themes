cc8cc114e35ba13f849c79a2ece5196450716dae
Inside4ndroid <inside4ndroid.techsup@gmail.com>
2024-10-10 10:48:20 +0100

    ignore git folders

--- generate_manifest.py
+++ generate_manifest.py
@@ -2,9 +2,13 @@ import os
 import json
 
 def generate_manifest():
+    # Define the directories to ignore
+    ignored_folders = {'.github', '.git'}
+    
     # Get a list of all items in the current directory
     root_dir = "."
-    folders = [f for f in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, f))]
+    folders = [f for f in os.listdir(root_dir) 
+               if os.path.isdir(os.path.join(root_dir, f)) and f not in ignored_folders]
 
     # Create a manifest as a JSON object
     manifest = {
